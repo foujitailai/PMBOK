2ec27b56223605c77a528c8f52a61509314f70f2
foujitailai <ebc@EBCdeMBP.lan>
2017-01-19 08:57:41 +0800

    pieces

---
+++ ConsoleTest/Game/Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+	/// <summary>
+	/// 整个系统的核心,中间的调度,就像大脑一样来管理\协调\接收\发送各种事务.
+	/// </summary>
+	public class Core
+	{
+		public Core ()
+		{
+			// TODO 设计它!!!
+
+			// 模块化\可拔插\可组合\可升级\可废除
+		}
+	}
+}
+
