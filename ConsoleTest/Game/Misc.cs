83096da01813524538fd40f9be276227a1db352d
foujitailai <ebc@EBCdeMBP.lan>
2017-02-03 22:09:28 +0800

    thinking in system
    应用系统之美里面的系统三要素,对模块化设计可以产生非常棒的效果!!! 目标working anywhere.

--- ConsoleTest/Game/Misc.cs
+++ ConsoleTest/Game/Misc.cs
@@ -4,16 +4,26 @@ namespace ConsoleTest
 {
 	namespace Misc
 	{
-		class Input{}
-		class Network{}
+		class Input { }
+		class Network { }
 
 		/// <summary>
 		/// 对各种系统的数据进行统计，各系统的性能分析，关键点设置数值输出-用于分析（就像在用万用表查电路一样）
 		/// 对于底层模块，先确定重要指标是什么，再确保它的稳定性、性能指标，读写文件的速度、次数、吞吐量等
 		/// </summary>
-		class Statistics{}
+		class Statistics { }
 
-		class Event{}
+		class Event { }
+
+		/// <summary>
+		/// 操作系统的虚拟内存与物理内存的管理,可以参考
+		/// </summary>
+		class FileSystem { }
+
+		/// <summary>
+		/// 内存\CPU\网络\显卡,什么都是资源,若有一个统一的管理方案,通过各种配置就能实现,那就NB了.
+		/// </summary>
+		class 资源调度系统 { }
 	}
 	
 }
